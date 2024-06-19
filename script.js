3d526ba4fe310cb1302c1e90a2268d417d2faef9
Harshita222 <124767756+Harshita222@users.noreply.github.com>
2024-06-19 22:57:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,3 @@
-// //your JS code here. If required.
-// 		 function updateSize() {
-//       const width = window.innerWidth;
-//       const height = window.innerHeight;
-//       const sizeText = document.getElementById('sizeText');
-//       sizeText.textContent = `Width: ${width} and Height: ${height}`;
-//     }
-// // 
-    
-//     updateSize();
-
-//     // Attach resize event listener to window
-//     window.addEventListener('resize', updateSize);
-
 function updateSize() {
     const width = window.innerWidth;
     const height = window.innerHeight;
